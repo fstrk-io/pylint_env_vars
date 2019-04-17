0a5029e4eb69f102c4e1ae41de945bff9ce75b27
Mike <mikhail.g.novikov@gmail.com>
2019-04-17 16:43:40 +0300

    положил egg в гит

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='pylint_env_vars',
     version='0.1',
     description='pylint_env_vars',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/fstrk-io/pylint_env_vars',
     author='Mikhail Novikov',
     author_email='mikhail.g.novikov@gmail.com',
